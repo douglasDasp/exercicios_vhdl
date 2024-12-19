d067fad02f841c2a69c7d35a710df1b0c3c67b6d
Douglas <35826621+douglasDasp@users.noreply.github.com>
2024-12-19 10:49:16 -0300

    Update led_blink.vhd
    testar o codigo na pratica FPGA PLACA ou simulacao

--- led_blink.vhd
+++ led_blink.vhd
@@ -14,19 +14,46 @@ use ieee.std_logic_unsigned.all;
 --definindo a entidade
 entity led_blink is
   port(
-    clk_50: in std_logic; -- 50Mhz
+    clk_50: in std_logic; -- clock de 50Mhz real que vem do Oscilador
     led: out std_logic;
     );
 end led_blink;
 
 -- definindo comportamento da entidade led_blink
 architecture behavioral of led_blink is
-  signal
-  signal
+  signal counter: std_logic_vector (24 downto 0);
+  signal clk_1: std_logic; -- clock de 1Hz "para alcançar"
+
+
+-- o processo definido abaixo, do tipo concorrente
+-- monitorar clk_50 e ver SE acontece a 'subida' do clk_50
+-- SE OK, entao VERIFICA SE counter < 25mil, que seria
+-- 50MHz / 2 = 25MHz no formato em binario
+-- SE OK, vai incrementando o "counter"
+-- SE NOK, clk_1 RECEBE seu INVERSO
+-- e counter vai para ZERO, "000000000000..." 
+-- com o "others" que vai 'mandar' zero para counter
 
 begin
+  prescaler: process(clk_50)
+    begin
+      if rising_edge(clk_50) then
+        if counter < "1011111010111100001000000" then
+          counter <= counter + 1;
+        else
+          clk_1 <= not clk_1;
+          counter <= (others => '0');
+        end if;
+      end if;
+    end process prescaler;
+    -- o led vai variar de acordo com que chegar no clk_1
+    led <= clk_1;
 
+
+
+  
 end behavioral;
+  
 
 
 
